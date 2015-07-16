e046eb6830d6c72a9d9eadc6fb515730d179b5e8
KL <x>
2015-07-16 09:15:52 -0700

    preparations1

---
+++ HexGL/WGL.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace HexTex.OpenGL {
+
+    class WGL {
+        public const uint PFD_DOUBLEBUFFER = 0x00000001;
+        public const uint PFD_STEREO = 0x00000002;
+        public const uint PFD_DRAW_TO_WINDOW = 0x00000004;
+        public const uint PFD_DRAW_TO_BITMAP = 0x00000008;
+        public const uint PFD_SUPPORT_GDI = 0x00000010;
+        public const uint PFD_SUPPORT_OPENGL = 0x00000020;
+        public const uint PFD_GENERIC_FORMAT = 0x00000040;
+        public const uint PFD_NEED_PALETTE = 0x00000080;
+        public const uint PFD_NEED_SYSTEM_PALETTE = 0x00000100;
+        public const uint PFD_SWAP_EXCHANGE = 0x00000200;
+        public const uint PFD_SWAP_COPY = 0x00000400;
+        public const uint PFD_SWAP_LAYER_BUFFERS = 0x00000800;
+        public const uint PFD_GENERIC_ACCELERATED = 0x00001000;
+        public const uint PFD_SUPPORT_DIRECTDRAW = 0x00002000;
+        public const uint PFD_DEPTH_DONTCARE = 0x20000000;
+        public const uint PFD_DOUBLEBUFFER_DONTCARE = 0x40000000;
+        public const uint PFD_STEREO_DONTCARE = 0x80000000;
+
+        public const byte PFD_TYPE_RGBA = 0;
+        public const byte PFD_TYPE_COLORINDEX = 1;
+
+        public const byte PFD_MAIN_PLANE = 0;
+        public const byte PFD_OVERLAY_PLANE = 1;
+        public const byte PFD_UNDERLAY_PLANE = 255;
+
+        [StructLayout(LayoutKind.Sequential)]
+        public class PIXELFORMATDESCRIPTOR {
+            public short nSize;
+            public short nVersion;
+            public uint dwFlags;
+            public byte iPixelType;
+            public byte cColorBits;
+            public byte cRedBits;
+            public byte cRedShift;
+            public byte cGreenBits;
+            public byte cGreenShift;
+            public byte cBlueBits;
+            public byte cBlueShift;
+            public byte cAlphaBits;
+            public byte cAlphaShift;
+            public byte cAccumBits;
+            public byte cAccumRedBits;
+            public byte cAccumGreenBits;
+            public byte cAccumBlueBits;
+            public byte cAccumAlphaBits;
+            public byte cDepthBits;
+            public byte cStencilBits;
+            public byte cAuxBuffers;
+            public byte iLayerType;
+            public byte bReserved;
+            public int dwLayerMask;
+            public int dwVisibleMask;
+            public int dwDamageMask;
+            public PIXELFORMATDESCRIPTOR() {
+                nSize = (short)Marshal.SizeOf(this);
+                nVersion = 1;
+                iPixelType = WGL.PFD_TYPE_RGBA;
+                cColorBits = 24;
+                cDepthBits = 32;
+                iLayerType = WGL.PFD_MAIN_PLANE;
+            }
+            public override string ToString() {
+                return string.Format("Flags=[{0}], PixelType={1}, ColorBits={2}, AplhaBits={3}, AccumBits={4}, AccumAlphaBits={5}, DepthBits={6}, StencilBits={7}, AuxBuffers={8}, LayerType={9}"
+                    , FlagsToString(), iPixelType == WGL.PFD_TYPE_RGBA ? "RGBA" : (iPixelType == WGL.PFD_TYPE_COLORINDEX ? "INDEX" : "?")
+                    , cColorBits, cAlphaBits, cAccumBits, cAccumAlphaBits, cDepthBits, cStencilBits, cAuxBuffers
+                    , iLayerType == WGL.PFD_MAIN_PLANE ? "MAIN" : (iLayerType == WGL.PFD_OVERLAY_PLANE ? "OVERLAY" : (iLayerType == WGL.PFD_UNDERLAY_PLANE ? "UNDERLAY" : "?")));
+            }
+            private string FlagsToString() {
+                System.Collections.Generic.List<string> list = new System.Collections.Generic.List<string>();
+                if ((dwFlags & WGL.PFD_DOUBLEBUFFER) != 0) list.Add("DOUBLEBUFFER");
+                if ((dwFlags & WGL.PFD_STEREO) != 0) list.Add("STEREO");
+                if ((dwFlags & WGL.PFD_DRAW_TO_WINDOW) != 0) list.Add("DRAW_TO_WINDOW");
+                if ((dwFlags & WGL.PFD_DRAW_TO_BITMAP) != 0) list.Add("DRAW_TO_BITMAP");
+                if ((dwFlags & WGL.PFD_SUPPORT_GDI) != 0) list.Add("SUPPORT_GDI");
+                if ((dwFlags & WGL.PFD_SUPPORT_OPENGL) != 0) list.Add("SUPPORT_OPENGL");
+                if ((dwFlags & WGL.PFD_GENERIC_FORMAT) != 0) list.Add("GENERIC_FORMAT");
+                if ((dwFlags & WGL.PFD_NEED_PALETTE) != 0) list.Add("NEED_PALETTE");
+                if ((dwFlags & WGL.PFD_NEED_SYSTEM_PALETTE) != 0) list.Add("NEED_SYSTEM_PALETTE");
+                if ((dwFlags & WGL.PFD_SWAP_EXCHANGE) != 0) list.Add("SWAP_EXCHANGE");
+                if ((dwFlags & WGL.PFD_SWAP_COPY) != 0) list.Add("SWAP_COPY");
+                if ((dwFlags & WGL.PFD_SWAP_LAYER_BUFFERS) != 0) list.Add("SWAP_LAYER_BUFFERS");
+                if ((dwFlags & WGL.PFD_GENERIC_ACCELERATED) != 0) list.Add("GENERIC_ACCELERATED");
+                if ((dwFlags & WGL.PFD_SUPPORT_DIRECTDRAW) != 0) list.Add("SUPPORT_DIRECTDRAW");
+                if ((dwFlags & WGL.PFD_DEPTH_DONTCARE) != 0) list.Add("DEPTH_DONTCARE");
+                if ((dwFlags & WGL.PFD_DOUBLEBUFFER_DONTCARE) != 0) list.Add("DOUBLEBUFFER_DONTCARE");
+                if ((dwFlags & WGL.PFD_STEREO_DONTCARE) != 0) list.Add("STEREO_DONTCARE");
+                return string.Join("|", list.ToArray());
+            }
+        }
+
+        [DllImport("gdi32.dll")]
+        public static extern int DescribePixelFormat(IntPtr hdc, int iPixelFormat, int nBytes, [In, Out] PIXELFORMATDESCRIPTOR ppfd);
+        [DllImport("gdi32.dll")]
+        public static extern int ChoosePixelFormat(IntPtr hdc, [In] PIXELFORMATDESCRIPTOR ppfd);
+        [DllImport("gdi32.dll")]
+        public static extern bool SetPixelFormat(IntPtr hdc, int iPixelFormat, PIXELFORMATDESCRIPTOR ppfd);
+        [DllImport("gdi32.dll")]
+        public static extern bool SwapBuffers(IntPtr hdc);
+
+        [DllImport("opengl32.dll", EntryPoint = "wglCreateContext")]
+        public static extern IntPtr CreateContext(IntPtr hdc);
+        [DllImport("opengl32.dll", EntryPoint = "wglDeleteContext")]
+        public static extern bool DeleteContext(IntPtr hglrc);
+        [DllImport("opengl32.dll", EntryPoint = "wglMakeCurrent")]
+        public static extern bool MakeCurrent(IntPtr hdc, IntPtr hglrc);
+        [DllImport("opengl32.dll", EntryPoint = "wglGetProcAddress")]
+        private static extern IntPtr GetProcAddress(String name);
+
+        [DllImport("User32.dll")]
+        public static extern IntPtr GetDC(IntPtr hwnd);
+        [DllImport("User32.dll")]
+        public static extern void ReleaseDC(IntPtr hwnd, IntPtr dc);
+
+        [DllImport("gdi32.dll")]
+        public static extern IntPtr CreateDC(string lpszDriver, string lpszDevice, string lpszOutput, IntPtr lpInitData);
+
+        private static Dictionary<Type, object> delegates = new Dictionary<Type, object>();
+        public static T GetDelegate<T>() {
+            var type = typeof(T);
+            object d = null;
+            if (!delegates.TryGetValue(type, out d)) {
+                var name = string.Concat("gl", type.Name);
+                var ptr = GetProcAddress(name);
+                if (ptr == IntPtr.Zero || ptr == new IntPtr(1) || ptr == new IntPtr(2)) throw new NotSupportedException(name);
+                d = Marshal.GetDelegateForFunctionPointer(ptr, type);
+                delegates.Add(type, d);
+            }
+            return (T)d;
+        }
+
+    }
+}
---
+++ SimpleDemo/DemoForm.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HexTex.OpenGL.SimpleDemo {
+
+    class DemoForm : Form {
+        DemoBase demo;
+        GLGraphics savedGL;
+        IntPtr savedHdc;
+        Point viewOffset = Point.Empty;
+        public DemoForm(DemoBase demo){
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+            this.TopMost = true;
+            this.SetStyle(ControlStyles.UserPaint, true);
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            //this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            //this.DoubleBuffered = true;
+                        this.MouseMove += new MouseEventHandler(TestFullScreen2_MouseMove);
+
+            this.demo = demo;
+        }
+        protected override void OnHandleCreated(EventArgs e) {
+            base.OnHandleCreated(e);
+            savedHdc = GLGraphics.GetDC(this.Handle);
+            savedGL = new GLGraphics(savedHdc, this.Size);
+            Console.WriteLine(string.Join("\n", savedGL.GetInfo()));
+            savedGL.Execute(x => {
+                demo.Prepare(x);
+            });
+        }
+        protected override void OnHandleDestroyed(EventArgs e) {
+            base.OnHandleDestroyed(e);
+            savedGL.Dispose();
+            GLGraphics.ReleaseDC(this.Handle, savedHdc);
+        }
+        protected override void OnPaintBackground(PaintEventArgs e) {
+            //base.OnPaintBackground(e);
+        }
+        protected override void OnPaint(PaintEventArgs e) {
+            //base.OnPaint(e);
+            if (savedGL == null) return;
+            savedGL.Execute(x => {
+                demo.Redraw(x);
+            });
+            this.Invalidate();
+            if (Control.MouseButtons == MouseButtons.Right) {
+                this.BeginInvoke(new MethodInvoker(delegate { this.Close(); }));
+            }
+        }
+        void TestFullScreen2_MouseMove(object sender, MouseEventArgs e) {
+            var p = this.Bounds.Location;
+            p.Offset(-this.Bounds.Width / 2, -this.Bounds.Height / 2);
+            p.Offset(e.Location);
+            viewOffset = p;
+            this.Invalidate();
+        }
+    }
+
+    class GLGraphics {
+        private IntPtr savedHdc;
+        private Size size;
+
+        public GLGraphics(IntPtr savedHdc, Size size) {
+            this.savedHdc = savedHdc;
+            this.size = size;
+        }
+        internal static IntPtr GetDC(IntPtr intPtr) {
+            throw new NotImplementedException();
+        }
+
+        internal string[] GetInfo() {
+            throw new NotImplementedException();
+        }
+
+        internal void Dispose() {
+            throw new NotImplementedException();
+        }
+
+        internal static void ReleaseDC(IntPtr intPtr, IntPtr savedHdc) {
+            throw new NotImplementedException();
+        }
+
+        internal void Execute(Action<IGL> action) {
+            throw new NotImplementedException();
+        }
+    }
+
+    abstract class DemoBase {
+        public abstract void Prepare(IGL gl);
+        public abstract void Redraw(IGL gl);
+    }
+}
