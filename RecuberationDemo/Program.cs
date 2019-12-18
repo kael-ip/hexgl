498728b883e49a7bc5ab8f2ab63ff72bf4321a6b
KL <@>
2019-12-19 00:35:23 +0300

    Loop demoreel

--- RecuberationDemo/Program.cs
+++ RecuberationDemo/Program.cs
@@ -3,9 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using HexTex.OpenGL;
 
 namespace HexTex.Recuberation {
     static class App {
+        static bool fullScreen = false;
+        static Type[] sequence = new Type[]{
+            typeof(Demo2),
+            typeof(Demo1),
+            typeof(DemoTest1)
+        };
+        static int current = 0, next = 0;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -13,9 +21,42 @@ namespace HexTex.Recuberation {
         static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            //Application.Run(new DemoForm(new HexTex.Recuberation.DemoTest1()));
-            //Application.Run(new DemoForm(new HexTex.Recuberation.Demo1()));
-            Application.Run(new DemoForm(new HexTex.Recuberation.Demo2()));
+            Application.Idle += Application_Idle;
+            Application.Run();
+        }
+        static void Application_Idle(object sender, EventArgs e) {
+            Application.Idle -= Application_Idle;
+            Run();
+        }
+        static void Run() {
+            if(next < 0) {
+                Application.Exit();
+                return;
+            } else {
+                current = next;
+            }
+            var demo = (DemoBase)Activator.CreateInstance(sequence[current]);
+            next = -1;
+            var form = new DemoForm(demo, fullScreen);
+            form.FormClosed += form_FormClosed;
+            form.KeyUp += form_KeyUp;
+            form.ShowDialog();
+        }
+        static void form_FormClosed(object sender, FormClosedEventArgs e) {
+            Application.Idle += Application_Idle;
+        }
+        static void form_KeyUp(object sender, KeyEventArgs e) {
+            if(e.KeyCode == Keys.Space) {
+                next = (current + 1) % sequence.Length;
+                ((Form)sender).Close();
+            } else if(e.KeyCode == Keys.Z) {
+                next = current;
+                ((Form)sender).Close();
+            } else if(e.KeyCode == Keys.F) {
+                next = current;
+                fullScreen = !fullScreen;
+                ((Form)sender).Close();
+            }
         }
     }
 }
