9d88913f6092c59f2af2bb1f11060c8a6a887ee3
KL <@>
2019-12-20 23:24:25 +0300

    Add Earth 3D Walk demo (promo)

--- RecuberationDemo/Program.cs
+++ RecuberationDemo/Program.cs
@@ -9,6 +9,7 @@ namespace HexTex.Recuberation {
     static class App {
         static bool fullScreen = false;
         static Type[] sequence = new Type[]{
+            typeof(Demo4),
             typeof(Demo3),
             typeof(Demo2),
             typeof(Demo1),
